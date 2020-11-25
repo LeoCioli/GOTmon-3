48e57c2c4cc213cc3bfb8eee75bc018d47e00e47
Corentinlfg <corentin.lafargue@edu.cece.fr>
2020-11-25 13:09:10 +0100

    Debut pour Collection

--- Collection.h
+++ Collection.h
@@ -1,8 +1,20 @@
-//
-// Created by Léo Cioli on 25/11/2020.
-//
-
-#ifndef GOTMON_3_COLLECTION_H
-#define GOTMON_3_COLLECTION_H
-
-#endif //GOTMON_3_COLLECTION_H
+#ifndef PROJETINFOING2_COLLECTION_H
+#define PROJETINFOING2_COLLECTION_H
+#include "registre.h"
+#include "carte.h"
+#include "creature.h"
+#include "attaque.h"
+#include "energie.h"
+class Collection {
+public:
+    Collection();
+    ~Collection();
+    void afficherCollection();
+    void setCreature(Creature creatureRecue);
+    std::vector<Creature> getCreature();
+    void setCarte(Carte *carteRecue);
+    std::vector<Carte*> getCarte();
+private:
+    std::vector<Carte*> m_collectionCarte;
+};
+#endif
