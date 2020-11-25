7c083523b70e638b46bccd8b8f27b8b43a307b7e
Corentinlfg <corentin.lafargue@edu.cece.fr>
2020-11-25 13:01:13 +0100

    Debut pour Creature

--- Creature.cpp
+++ Creature.cpp
@@ -1,4 +1,37 @@
 //
-// Created by Léo Cioli on 25/11/2020.
+// Created by Corentin Lafargue on 23/11/2020.
 //
+
+#include "Creature.h"
+#include "Attaque.h"
+
+Creature::Creature() {
+}
+
+Creature::Creature(std:: string name, int vie) {
+}
+
+Creature:: ~Creature() {
+}
+
+void Creature::recevoirDegat(Attaque &attaque){
+    SetVie(GetVie() - attaque.GetPuissance());
+}
+
+void Creature::SetVie(int vie) {
+    m_vie = vie;
+}
+
+int Creature::GetVie() const {
+    return m_vie;
+}
+
+void Creature::afficher() {
+    std:: cout << "Votre créature est " << m_nom << " et, il lui reste  " << m_vie << " de vie." << std:: endl;
+}
+void Creature::setCreature(std::string nom, int vie) {
+    nom = m_nom;
+    vie = m_vie;
+}
+
 
--- Creature.h
+++ Creature.h
@@ -1,8 +1,38 @@
 //
-// Created by Léo Cioli on 25/11/2020.
+// Created by Corentin Lafargue on 23/11/2020.
 //
 
-#ifndef GOTMON_3_CREATURE_H
-#define GOTMON_3_CREATURE_H
+#ifndef PROJET_CREATURE_H
+#define PROJET_CREATURE_H
 
-#endif //GOTMON_3_CREATURE_H
+#include <iostream>
+#include <string>
+
+#include "Attaque.h"
+#include "Carte.h"
+
+
+class Creature {
+
+public:
+
+    Creature();
+    Creature(std :: string nom, int vie);
+    ~Creature();
+    void recevoirDegat(Attaque &attaque);
+    void afficher ();
+    void SetVie(int vie);
+    int GetVie() const;
+    void setCreature(std:: string nom, int vie);
+
+
+
+protected:
+
+    std:: string m_nom;
+    int m_vie;
+
+};
+
+
+#endif //PROJET_CREATURE_H
