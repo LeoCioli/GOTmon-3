6126980e2afa992b8d11f2ed0d2c10270ee4db24
Corentinlfg <corentin.lafargue@edu.cece.fr>
2020-11-25 12:45:26 +0100

    Debut pour Energie

--- Energie.cpp
+++ Energie.cpp
@@ -2,3 +2,36 @@
 // Created by Léo Cioli on 25/11/2020.
 //
 
+//
+// Created by Corentin Lafargue on 24/11/2020.
+//
+
+#include "Energie.h"
+#include "Carte.h"
+#include <iostream>
+
+Energie::Energie() {
+}
+Energie::~Energie() {
+}
+Energie::Energie(int niveau, std::string type) {
+}
+
+void Energie :: setType(std::string type) {
+    m_type = type;
+}
+
+int Energie::setNiveau(int niveau) {
+    m_niveau = niveau;
+}
+
+int Energie::GetNiveau() const {
+    return m_niveau;
+}
+
+
+void Energie::SetEnergie(int niveau, std::string type) {
+    m_niveau = niveau;
+    m_type = type;
+}
+
--- Energie.h
+++ Energie.h
@@ -1,8 +1,27 @@
-//
-// Created by Léo Cioli on 25/11/2020.
-//
 
-#ifndef GOTMON_3_ENERGIE_H
-#define GOTMON_3_ENERGIE_H
+#ifndef PROJET_ENERGIE_H
+#define PROJET_ENERGIE_H
 
-#endif //GOTMON_3_ENERGIE_H
+#include <iostream>
+#include <string>
+#include "Carte.h"
+
+class Energie : public Carte{
+
+public:
+    Energie();
+    ~Energie();
+    Energie(int niveau, std::string type);
+    void SetEnergie(int niveau, std:: string type);
+    int setNiveau(int niveau);
+    int GetNiveau() const;
+    void setType(std:: string type);
+    void GetType() const;
+
+
+protected:
+    std::string m_niveau;
+    std::string m_type;
+};
+
+#endif
